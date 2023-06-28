f4ea85a2ccf6257ee229ec27360939106cf19d8f
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-06-28 09:33:38 -0300

    "arreglo nav"

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,8 @@ import CartWidget from "../CartWidget/CartWidget"
 import './NavBar.css'
 import { NavLink, Link } from "react-router-dom"
 
+
+
 const NavBar = () => {
     const imgTD = "https://tomasgonzalez77.github.io/tomasgonzalez-proyectofinal/image/dondequiero.png";
     return (
@@ -9,9 +11,9 @@ const NavBar = () => {
             <Link to={"/"}>
                 <img className="imgTD" src={imgTD} alt="Logo Todo Padel" />
             </Link>
-
+    
             <nav>
-                <ul>
+                <ul className="nav-list">
                     <li>
                         <NavLink className="nav" activeClassName="active" to={`/categoria/paletas`}> Paletas </NavLink>
                     </li>
@@ -33,5 +35,7 @@ const NavBar = () => {
         </header>
     )
 }
+
+
 
 export default NavBar
