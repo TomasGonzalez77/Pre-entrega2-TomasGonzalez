2cbf2bc57a67c3a830d6838a9ee47bf8e949e308
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-07-02 12:57:33 -0300

    "correccion import"

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,15 +2,15 @@ import { useContext } from "react"
 import { CarritoContext } from "../../Context/CarritoContext"
 import "./CartItem.css"
 
-const CartItem = ({item, cantidad}) => {
-    const {eliminarProducto} = useContext(CarritoContext);
+const CartItem = ({ item, cantidad }) => {
+  const { eliminarProducto } = useContext(CarritoContext);
   return (
     <div>
-        <h4> {item.nombre} </h4>
-        <p>Cantidad: {cantidad} </p>
-        <p>Precio: {item.precio} </p>
-        <button className="btnEliminar" onClick={()=> eliminarProducto(item.id)}> Eliminar </button>
-        <hr />
+      <h4> {item.nombre} </h4>
+      <p>Cantidad: {cantidad} </p>
+      <p>Precio: {item.precio} </p>
+      <button className="btnEliminar" onClick={() => eliminarProducto(item.id)}> Eliminar </button>
+      <hr />
     </div>
   )
 }
